368575f55c4a19902f0fe69b9b8f8c5e8fa4540b
MOHAMAD KADIK <mohamadkhadik2@gmail.com>
2026-06-05 14:27:22 +0700

    Initialize Flask app with a hello route

---
+++ UAS_2388010015/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>UAS Administrasi Server Berhasil!</h1><p>Aplikasi Python Dinamis berjalan di Docker.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
