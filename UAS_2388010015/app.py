afafbb86dc7c5900a116fdd049090c114570226b
MOHAMAD KADIK <mohamadkhadik2@gmail.com>
2026-06-05 17:00:12 +0700

    Initialize Flask app for dynamic Python application

--- UAS_2388010015/app.py
+++ UAS_2388010015/app.py
@@ -1,1 +1,11 @@
-return "<h1>UAS Administrasi Server Berhasil!</h1><p>Nama: Mohamad Kadik</p><p>NIM: 2388010015</p><p>Aplikasi Python Dinamis berjalan di Docker.</p>"
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>UAS Administrasi Server Berhasil!</h1><p>Nama: Mohamad Kadik</p><p>NIM: 2388010015</p><p>Aplikasi Python Dinamis berjalan di Docker.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
